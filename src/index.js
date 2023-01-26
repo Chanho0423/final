ae41db645b5977a9f74c914b64af788001f11c76
Chanho0423 <adamant@postech.ac.kr>
2023-01-26 21:44:58 +0900

    final

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Nav from './Nav';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Nav />
     </BrowserRouter>
   </React.StrictMode>
